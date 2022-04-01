0e9cb069b12d36101c36e2acb7f5a33a099c9329
zauzooz <nnt010802@gmail.comm>
2022-04-01 23:36:32 +0700

    Online Password Manager Application

---
+++ Server/Server/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main()
+        {
+            Executive();
+        }
+        private void Authentication()
+        { }
+        private static void Executive()
+        {
+            // create a endpoint
+            IPHostEntry ipHostEntry = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = ipHostEntry.AddressList[0];
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, 15000);
+
+            // creat socket
+            Socket socket = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            // bind the endpoint
+            socket.Bind(ipEndPoint);
+
+            socket.ReceiveTimeout = 10;
+            socket.SendTimeout = 10;
+
+            socket.Listen(socket.ReceiveTimeout);
+
+            while (true)
+            {
+                Console.WriteLine("Password Manager Server");
+                break;
+            }    
+
+            socket.Close();
+        }
+    }
+}
