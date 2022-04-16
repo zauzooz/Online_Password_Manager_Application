b106eafc0c831ea7fcfc566d7026e426c2f2422c
zauzooz <nnt010802@gmail.comm>
2022-04-16 14:48:10 +0700

    16/4/2022

---
+++ Console_Client/Console_Client/Console_Client/Program.cs
@@ -0,0 +1,115 @@
+﻿// A C# program for Client
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+
+	class Client
+	{
+
+		static private IPHostEntry ipHost = null;
+		static private IPAddress ipAddr = null;
+		static private IPEndPoint localEndPoint = null;
+		static private Socket sender = null;
+
+		// Main Method
+		static void Main(string[] args)
+		{
+			ExecuteClient();
+		}
+
+		static void EndPoint()
+        {
+			ipHost = Dns.GetHostEntry(Dns.GetHostName());
+			ipAddr = ipHost.AddressList[0];
+			localEndPoint = new IPEndPoint(ipAddr, 15000);
+			sender = new Socket(ipAddr.AddressFamily,
+						SocketType.Stream, ProtocolType.Tcp);
+		}
+
+		static void Connect()
+        {
+			sender.Connect(localEndPoint);
+		}
+
+		static void Send(string msg)
+        {
+			byte[] messageSent = Encoding.ASCII.GetBytes(msg);
+			int byteSent = sender.Send(messageSent);
+		}
+
+		private static string Recieve()
+        {
+			byte[] messageReceived = new byte[1024];
+			string data = null;
+			
+			int byteRecv = sender.Receive(messageReceived);
+			data = Encoding.ASCII.GetString(messageReceived,0,byteRecv);
+
+			return data;
+			
+		}
+
+		private static void Close()
+        {
+			sender.Shutdown(SocketShutdown.Both);
+			sender.Close();
+		}
+
+		// ExecuteClient() Method
+		static void ExecuteClient()
+		{
+			try
+			{
+				EndPoint();
+
+				try
+				{
+
+					Connect();	
+
+					Console.WriteLine("Socket connected to -> {0} ",
+								sender.RemoteEndPoint.ToString());
+
+					Send("LOGIN:user:pass");
+
+					// Data buffer
+					string data = Recieve();
+					Console.WriteLine("Message from Server -> {0}",
+						data);
+
+					Send("END CONNECTION:");
+					Close();
+					
+				}
+
+				// Manage of Socket's Exceptions
+				catch (ArgumentNullException ane)
+				{
+
+					Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+				}
+
+				catch (SocketException se)
+				{
+
+					Console.WriteLine("SocketException : {0}", se.ToString());
+				}
+
+				catch (Exception e)
+				{
+					Console.WriteLine("Unexpected exception : {0}", e.ToString());
+				}
+			}
+
+			catch (Exception e)
+			{
+
+				Console.WriteLine(e.ToString());
+			}
+		}
+	}
+}
--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -3,24 +3,132 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
-using System.Data.SqlClient;
 
 namespace Server
 {
     class Program
     {
+        // create an endpoint
+        //ipHostEntry = Dns.GetHostEntry(Dns.GetHostName());
+        //ipAddress = ipHostEntry.AddressList[0];
+        //ipEndPoint = new IPEndPoint(ipAddress, 15000);
+        static private IPHostEntry ipHostEntry = null;
+        static private IPAddress ipAddress = null;
+        static private IPEndPoint ipEndPoint = null;
+        // create a TCP socket:  listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp); 
+        static private Socket listener = null;
+        // accept a connection: clientSocket = listenner.Accept()
+        static private Socket clientSocket = null;
+
+        static void EndPoint()
+        {
+            // create an endpoint
+            ipHostEntry = Dns.GetHostEntry(Dns.GetHostName());
+            ipAddress = ipHostEntry.AddressList[0];
+            ipEndPoint = new IPEndPoint(ipAddress, 15000);         
+        }
+
+        static void _Socket()
+        {
+            // creat a TCP socket
+            listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        static void Bind()
+        {
+            // bind the endpoint
+            listener.Bind(ipEndPoint);
+        }
+        
+        static void Listen()
+        {
+            listener.Listen();
+        }
+
+        static void Accept()
+        {
+            clientSocket = null;
+            clientSocket = listener.Accept();
+        }
+
+        static void Send(string msg = "respone") 
+        {
+            byte[] message = Encoding.ASCII.GetBytes(msg);
+            clientSocket.Send(message);
+        }
+
+        static string Recieve()
+        {
+            // data buffer
+            string data = null;
+            byte[] bytes = new byte[1024];
+
+            while (true)
+            {
+                int numByte = clientSocket.Receive(bytes);
+
+                data += Encoding.ASCII.GetString(bytes,
+                                           0, numByte);
+
+                if (data.IndexOf("\0") > -1)
+                    break;
+            }
+
+            return data;
+        }
+
+        static void Close() 
+        {
+            clientSocket.Shutdown(SocketShutdown.Both);
+            clientSocket.Close();
+            listener.Close();
+        }
+
         static void Main()
         {
             Executive();
         }
-        private void doEvent(string usernameAndPassword)
-        { 
-            string[] subs = usernameAndPassword.Split(':');
+
+        static private void LOGIN(string[] str)
+        {
+            string username = str[1];
+            string password = str[2];
+            if(username =="user" && password == "pass")
+            {
+                Send("LOGIN SUCCESSFULL:your vault here");
+            }
+            else
+            {
+                Send("Username or password is incorrect.");
+            }                
+        }
+
+        private void CREATE_ACCOUNT()
+        { }
+
+        private void DELETE_ACCOUNT()
+        { }
+
+        private void SYNCHRONIZATION()
+        { }
+
+        private static void END_CONNECTION()
+        {
+            Send("Close connection.");
+            Close();
+        }
+
+
+        static private void doEvent()
+        {
+            string data = Recieve();
+            string[] subs = data.Split(':');
             switch (subs[0])
             {
                 // Đăng nhập, nếu thành công thì gửi vault, không thì báo nhập sai
                 case "LOGIN":
                     Console.WriteLine("LOGIN");
+                    LOGIN(subs);
                     break;
                 // Tạo tài khoản trên hệ thống lưu trữ mật khẩu
                 case "CREATE ACCOUNT":
@@ -37,37 +145,30 @@ namespace Server
                 // Ngắt kết nối
                 case "END CONNECTION":
                     Console.WriteLine("END CONNECTION");
+                    END_CONNECTION();
                     break;
                 default:
                     Console.WriteLine("END");
                     break;
             }    
         }
+
         private static void Executive()
         {
-            // create a endpoint
-            IPHostEntry ipHostEntry = Dns.GetHostEntry(Dns.GetHostName());
-            IPAddress ipAddress = ipHostEntry.AddressList[0];
-            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, 15000);
 
-            // creat socket
-            Socket socket = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            EndPoint();
 
-            // bind the endpoint
-            socket.Bind(ipEndPoint);
+            _Socket();
 
-            socket.ReceiveTimeout = 10;
-            socket.SendTimeout = 10;
+            Bind();
 
-            socket.Listen(socket.ReceiveTimeout);
+            Listen();
 
             while (true)
-            {
-                Console.WriteLine("Password Manager Server");
-                break;
+            { 
+                Accept();
+                doEvent();
             }    
-
-            socket.Close();
         }
     }
 }
